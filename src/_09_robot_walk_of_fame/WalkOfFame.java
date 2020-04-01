093559cfb0775ed184f611895f477c5985b676ee
C2SDK <C2SDK@C47508>
2020-04-01 14:57:49 -0700

    Walk of Fame

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -9,12 +9,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		 rob.setX(60);
+		 rob.penDown(); 
+		 rob.move(30);
+		 rob.turn(144);
+		 rob.move(30);
+		 rob.turn(144);
+		 rob.move(30);
+		 rob.turn(144);
+		 rob.move(30);
+		 rob.turn(144);
+		 rob.move(30);
+		 rob.turn(144);
+		 rob.move(30);
+		 rob.move(1000);
+		// 1. Set the X position of the robot so that                                     it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 
 		// 2. Make the robot draw a star shape. Hint: 144.
 
-		// 3. Set the size of the star to 30.
+		// 3. Set the size of the star to 30.                  
 
 		/** THE CHALLENGE: **/
 		/*
